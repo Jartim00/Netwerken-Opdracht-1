bef6bd1d958733e2c41d42a658113e8608eea9dc
Jaron Timmerman <jarontimmerman@live.nl>
2018-02-22 15:47:15 +0100

    naming changed

--- rename.js
+++ rename.js
@@ -21,27 +21,27 @@ var server = http.createServer(function (req, res) {
     res.end(); 
   }).listen(8002)
 var server = http.createServer(function (req, res) {
-    var html = '<!DOCTYPE html><html><body style="background-color:purple;"><h1>This is my first header.</h1><p>Poort 8002</p></body></html>'
+    var html = '<!DOCTYPE html><html><body style="background-color:purple;"><h1>This is my first header.</h1><p>Poort 8003</p></body></html>'
     res.write(html); 
     res.end(); 
   }).listen(8003)
 var server = http.createServer(function (req, res) {
-    var html = '<!DOCTYPE html><html><body style="background-color:pink;"><h1>This is my first header.</h1><p>Poort 8002</p></body></html>'
+    var html = '<!DOCTYPE html><html><body style="background-color:pink;"><h1>This is my first header.</h1><p>Poort 8004</p></body></html>'
     res.write(html); 
     res.end(); 
   }).listen(8004)
 var server = http.createServer(function (req, res) {
-    var html = '<!DOCTYPE html><html><body style="background-color:grey;"><h1>This is my first header.</h1><p>Poort 8002</p></body></html>'
+    var html = '<!DOCTYPE html><html><body style="background-color:grey;"><h1>This is my first header.</h1><p>Poort 8005</p></body></html>'
     res.write(html); 
     res.end(); 
   }).listen(8005)
 var server = http.createServer(function (req, res) {
-    var html = '<!DOCTYPE html><html><body style="background-color:white;"><h1>This is my first header.</h1><p>Poort 8002</p></body></html>'
+    var html = '<!DOCTYPE html><html><body style="background-color:white;"><h1>This is my first header.</h1><p>Poort 8006</p></body></html>'
     res.write(html); 
     res.end(); 
   }).listen(8006)
 var server = http.createServer(function (req, res) {
-    var html = '<!DOCTYPE html><html><body style="background-color:orange;"><h1>This is my first header.</h1><p>Poort 8002</p></body></html>'
+    var html = '<!DOCTYPE html><html><body style="background-color:orange;"><h1>This is my first header.</h1><p>Poort 8007</p></body></html>'
     res.write(html); 
     res.end(); 
   }).listen(8007)
