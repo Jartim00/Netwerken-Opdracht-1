a06991009ed637ab4bcc5dc7c76ebc641edd4569
Jurjen <haze1401@student.nhl.nl>
2018-02-22 16:22:37 +0100

    Sockets staan nu op een timer, zullen automatisch sluiten

--- PortForwarder.py
+++ PortForwarder.py
@@ -15,22 +15,21 @@ def transfer(src, dst, direction):
     dst_name = dst.getsockname()
     dst_address = dst_name[0]
     dst_port = dst_name[1]
+    src.settimeout(1)
+	
     while True:
-        buffer = src.recv(0x400)
-        if len(buffer) == 0:
-            print "[-] No data received! Breaking..."
+        try:
+            buffer = src.recv(0x400)
+            if len(buffer) == 0:
+                    break
+            dst.send(handle(buffer))
+        except Exception as inst:
+            print "Ip released" 
             break
-        # print "[+] %s:%d => %s:%d [%s]" % (src_address, src_port, dst_address, dst_port, repr(buffer))
-        if direction:
-            print "[+] %s:%d >>> %s:%d [%d]" % (src_address, src_port, dst_address, dst_port, len(buffer))
-        else:
-            print "[+] %s:%d <<< %s:%d [%d]" % (dst_address, dst_port, src_address, src_port, len(buffer))
-        dst.send(handle(buffer))
     try:
-        print "[+] Closing src connecions! [%s:%d]" % (src_address, src_port)
         src.shutdown(socket.SHUT_RDWR)
         src.close()    
-        print "[+] Closing dest connecions! [%s:%d]" % (dst_address, dst_port)
+
         dst.shutdown(socket.SHUT_RDWR)
         dst.close()
     except Exception as inst:
@@ -42,20 +41,16 @@ def server(local_host, local_port, remote_host, remote_port, max_connection):
     
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
     server_socket.bind((local_host, local_port))
     server_socket.listen(max_connection)
 
 
-    print '[+] Server started [%s:%d]' % (local_host, local_port)
+    print 'Portforwarder started'
     while True:
-        print "Current loadBalanceIndex is:" + str(loadBalanceIndex)
-        print "remote length " + str(len(remote_port))
-        print "adding" + str((loadBalanceIndex + 1) % len(remote_port))
-
-
         local_socket, local_address = server_socket.accept()
         remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        print "trying to connect to remote port:" + str(remote_port[loadBalanceIndex])
+
         remote_socket.connect((remote_host, remote_port[loadBalanceIndex]))
         loadBalanceIndex = (loadBalanceIndex + 1) % len(remote_port)
 
@@ -67,24 +62,21 @@ def server(local_host, local_port, remote_host, remote_port, max_connection):
         s.start()
         r.start()
 
-    print "[+] Releasing resources..."
     remote_socket.shutdown(socket.SHUT_RDWR)
     remote_socket.close()
+
     local_socket.shutdown(socket.SHUT_RDWR)
     local_socket.close()
-    print "[+] Closing server..."
+
     server_socket.shutdown(socket.SHUT_RDWR)
     server_socket.close()
-    print "[+] Server shuted down!"
-
 
 def main():
-    LOCAL_HOST = "141.252.218.55"
+    LOCAL_HOST = "127.0.0.1"#"141.252.218.55"
     LOCAL_PORT = 8080
-    REMOTE_HOST = "141.252.207.174" #raw_input("IP? ") 
-    print REMOTE_HOST
+    REMOTE_HOST = "127.0.0.1" #raw_input("IP? ") 
     test = str(REMOTE_HOST)
-    REMOTE_PORTS = [8888, 8889]
+    REMOTE_PORTS = [8001, 8002, 8003, 8004, 8005, 8006, 8007]
     MAX_CONNECTION = 0x10
     server(LOCAL_HOST, LOCAL_PORT, test, REMOTE_PORTS, MAX_CONNECTION)
 
